54722bd1cb311ec0026a40f7bc2812a8928bce4b
Lars Pind <lars@pinds.com>
2003-09-18 10:43:12 +0000

    Log delete confirm page

--- www/log-delete.tcl
+++ www/log-delete.tcl
@@ -5,11 +5,29 @@ ad_page_contract {
     @creation-date 2003-04-24
     @cvs-id $Id$
 } {
-    entry_id:integer
+    entry_id:integer,multiple
+    {confirm_p:boolean 0}
 }
 
-permission::require_permission -object_id $entry_id -privilege delete
+if { !$confirm_p } {
+    set num_entries [llength $entry_id]
 
-logger::entry::delete -entry_id $entry_id
+    if { $num_entries == 0 } {
+        ad_returnredirect .
+        return
+    }
 
+    set page_title "Delete Log [ad_decode $num_entries 1 "Entry" "Entries"]"
+    set context [list $page_title]
+    set yes_url "log-delete?[export_vars { entry_id:multiple { confirm_p 1 } }]"
+    set no_url "."
+
+    return
+}
+
+foreach entry_id $entry_id {
+    permission::require_permission -object_id $entry_id -privilege delete
+    logger::entry::delete -entry_id $entry_id
+}
+    
 ad_returnredirect .
